36477361b1ec15f36c21695535ca46113b03cf5f
Gasper Habjan <gapy5000@gmail.com>
2024-01-07 11:28:52 +0100

    Organize structure of the repo.

---
+++ code/create_video.py
@@ -0,0 +1,21 @@
+import moviepy.video.io.ImageSequenceClip	
+import os
+import re
+
+image_folder = './figures/'
+video_name = "sheepherding.mp4"
+ 
+
+fps=5
+
+
+
+# List images
+image_files = [os.path.join(image_folder,img)
+               for img in os.listdir(image_folder)
+               if img.endswith(".png")]
+# Sort by number
+image_files.sort(key=lambda x: int(re.sub(r'./figures/(\d+)\.png', r'\1', x)))
+
+clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+clip.write_videofile(video_name)
---
+++ code/runner.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+from sheep import Sheep
+from shepherd import ShepherdDog
+import configparser
+import time
+
+
+# Import config file from config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Read parameters from config file
+parameters = {key: float(p.split()[0]) for key, p in config['PARAMETERS'].items()}
+
+goal = [int(config['GOAL']['X']), int(config['GOAL']['Y']), int(config['GOAL']['RAD'])]
+
+# Setup starting positions
+dog = ShepherdDog(config['DOG']['X'], config['DOG']['Y'], goal, parameters)
+
+sheep_arr = []
+
+sheep_count = int(config['SHEEP']['N'])
+for i in range(1, sheep_count + 1):
+    curr_sheep_x, curr_sheep_y = config['SHEEP'][f'p{i}'].split(',')
+    sheep_obj = Sheep(int(curr_sheep_x), int(curr_sheep_y), goal, parameters)
+    sheep_obj.goal_not_reached = True
+    sheep_arr.append(sheep_obj)
+
+success = False
+step = 0
+while step != 4000 and not success:
+
+    if not step % 2:
+        print(f"Step: {step}")
+        # Plot the current positions
+        fig = plt.figure()
+        ax = fig.subplots()
+
+        width = int(config['WINDOW']['WIDTH'])
+        height = int(config['WINDOW']['HEIGHT'])
+
+        # Setup plot limits
+        ax.set_xlim(-100, width-100)
+        ax.set_ylim(0, height)
+
+        # Setup ticks to show grid
+        ax.set_xticks(range(-50, int(config['WINDOW']['WIDTH']), 50))
+        ax.set_yticks(range(-50, int(config['WINDOW']['HEIGHT']), 50))
+
+        # Show grid with reduced alpha
+        ax.grid(alpha=0.25)
+
+        ax.scatter(dog.position[0], dog.position[1], color='red', marker='o', s=25)
+
+        # Draw sheep on plot
+        ax.scatter([sheep_arr[i].position[0] for i in range(len(sheep_arr))], [
+                sheep_arr[i].position[1] for i in range(len(sheep_arr))], color='blue', marker='o', s=20)
+        
+        # Draw the goal circle
+        circle = plt.Circle((int(config['GOAL']['X']), int(config['GOAL']['Y'])), int(
+            config['GOAL']['RAD']), color='r', fill=False)
+        
+        ax.add_artist(circle)
+
+        fig.savefig(f'./figures/{step}.png')
+
+        plt.close()
+
+
+    if dog.calculate_velocity(sheep_arr):
+        # print("Success at step: ", step)
+        break
+    dog.move()
+
+    # Move sheep
+    for sheep in sheep_arr:
+        sheep.calculate_velocity(dog, sheep_arr, step)
+        sheep.move()
+
+
+
+
+
+    step += 1
+
